fca3c4b20cb22a3f2b78688fd4e7413d03e978e0
cBrayton <charlie.brayton@gmail.com>
2018-09-05 12:10:37 -0700

    Update archetypeCrawler.js

--- archetypeCrawler.js
+++ archetypeCrawler.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
-import {BootstrapTable, 
-       TableHeaderColumn} from 'react-bootstrap-table';
+//import ReactDOM from 'react-dom';
+//import React, { Component } from 'react';
+//import {BootstrapTable, 
+//       TableHeaderColumn} from 'react-bootstrap-table';
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
