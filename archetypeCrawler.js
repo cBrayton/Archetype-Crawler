fff92632740fa26a99609414cf6c69308e950ff4
cBrayton <charlie.brayton@gmail.com>
2018-09-06 13:10:30 -0700

    Update archetypeCrawler.js

--- archetypeCrawler.js
+++ archetypeCrawler.js
@@ -9,6 +9,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var data;
+var flavor;
+var pairs;
 
 //This data needs to be replaced with the archetypedata cache
 //and the archetypeflavor cache filtered by the selected class
@@ -90,6 +92,45 @@ var ClassSelector = function (_React$Component2) {
       this.setState({
         value: e.target.value
       });
+      console.log(this.state.value);
+      var archearr;
+      var flavorarr;
+      for(var i = 0; i < data["keys-0"].length; i++) {
+        if(this.state.value === data["keys-0"][i]["key-0"]) {
+          archearr = data["keys-0"][i]["hash-1"]["keys-1"];
+          archearr.sort(function(a,b) {
+            if (a < b) {
+              return -1;
+            }
+            if (a > b) {
+              return 1;
+            }
+            return 0;
+          });
+        }
+      }
+      for(var i = 0; i < flavor["keys-0"].length; i++) {
+        if(this.state.value === flavor["keys-0"][i]["key-0"]) {
+          flavorarr = flavor["keys-0"][i]["hash-1"]["keys-1"];
+          flavorarr.sort(function(a,b) {
+            if (a < b) {
+              return -1;
+            }
+            if (a > b) {
+              return 1;
+            }
+            return 0;
+          });
+        }
+      }
+      //archearr and flavorarr should be equal length
+      console.log(archearr.length === flavorarr.length);
+      for(var j = 0; j < archearr.length; j++) {
+            iData[j][id] = j;
+            iData[j][name] = archearr[j]["key-1"];
+            iData[j][reqs] = archearr[j]["set-2"].join(", ");
+            iData[j][desc] = flavorarr[j]["text"];
+      }
     }
   }, {
     key: "render",
@@ -148,13 +189,16 @@ var domContainerTable = document.querySelector('#archetype_table_js');
 ReactDOM.render(React.createElement(ArchetypeTable, { data: iData }), domContainerTable);
 var domContainerSelector = document.querySelector('#class_selector_js');
 ReactDOM.render(React.createElement(ClassSelector, null), domContainerSelector);
-console.log(domContainerSelector.innerHTML);
 
 axios.get("archetypedataCache.json").then(function (result) {
   console.log(result);
   data = result.data;
   updateOptions(data);
   console.log(data);
-  console.log("In axios call");
   ReactDOM.render(React.createElement(ClassSelector, null), domContainerSelector);
+});
+
+axios.get("archetypeflavor.json").then(function (result) {
+  console.log(result);
+  flavor = result.data;
 });
